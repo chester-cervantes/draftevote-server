7410ad2dc99c3212f98c2f25d48d2cfe2109e558
ecbob <chestercervantes13@hotmail.com>
2021-02-20 00:50:13 -0800

    - remove test db with then catch

--- models/index.js
+++ models/index.js
@@ -3,13 +3,7 @@ const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATABASE,
-    {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
-    console.log('start');
-})
-    .catch(err => {
-        console.error('App starting error:', err.stack);
-        process.exit(1)
-    });
+    { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports.User = require('./user');
 module.exports.Poll = require('./poll');
