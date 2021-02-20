115bcc8f8b62069748dae19f94d4744da8fa318f
ecbob <chestercervantes13@hotmail.com>
2021-02-20 00:41:41 -0800

    - test db with then catch

--- models/index.js
+++ models/index.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATABASE,
-    { useNewUrlParser: true, useUnifiedTopology: true });
+    {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log('start');
+})
+    .catch(err => {
+        console.error('App starting error:', err.stack);
+        process.exit(1)
+    });
 
 module.exports.User = require('./user');
 module.exports.Poll = require('./poll');
